395a5f3c6b7f614fa93b94b8ee8bad7fd9baa863
mektigboy <mektigboy@protonmail.com>
2022-08-16 16:19:26 -0500

    Update deploy/01-deploy-binaries-nft.js

--- deploy/01-deploy-binaries-nft.js
+++ deploy/01-deploy-binaries-nft.js
@@ -1,24 +1,27 @@
+const { developmentChains } = require("../helper-hardhat-config");
 const { network } = require("hardhat");
 const { verify } = require("../utils/verify");
 
 module.exports = async function ({ deployments, getNamedAccounts }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
-
-    log("--------------------------------------------------");
-
+    const arguments = [];
     const binariesNFT = await deploy("BinariesNFT", {
-        args: [],
+        args: arguments,
         from: deployer,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
-    if (!chainId == 31337 && process.env.ETHERSCAN_API_KEY) {
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
         log("Verifying...");
         await verify(binariesNFT.address, arguments);
     }
+
+    log("--------------------------------------------------");
 };
 
 module.exports.tags = ["all", "binaries-nft"];
